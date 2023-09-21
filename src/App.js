70c40e83fd55ab05bd9a5897e1aac216f615a7ca
Vaibhav Kumar <vaibhavkr87097@gmail.com>
2023-09-21 15:16:07 +0530

    e-commerce created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./component/Navbar/Navbar";
+import Footer from "./component/Footer/Footer";
+import Hero from "./component/Hero/Hero"
+
+function App() {
+  return (
+    <div className="App">
+      
+<Navbar/>
+<Hero/>
+<Footer/>
+
+    </div>
+  );
+}
+
+export default App;
